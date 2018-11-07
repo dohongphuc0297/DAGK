3322a5668d4273c346789d9333ee8e4097bf5ea6
PHUC-PC\dohongphuc <dohongphuc1997@gmail.com>
2018-11-07 20:43:44 +0700

    thêm link user trên header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,23 +2,26 @@ import React from 'react';
 import { logOut } from "../actions/index";
 import { connect } from 'react-redux';
 
-const Header = ({logout}) =>{
+const Header = (props) =>{
     return(
         <nav className="navbar navbar-default">
-                <div className="navbar-header">
+        <div className="container">
+        <div className="navbar-header">
                     <button type="button" className="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
                         <span className="sr-only">Toggle navigation</span>
                         <span className="icon-bar"></span>
                         <span className="icon-bar"></span>
                         <span className="icon-bar"></span>
                     </button>
-                    <a className="navbar-brand" href="/">CHAT</a>
+                    <a className="navbar-brand" href="#">CHAT WITH FIREBASE</a>
                 </div>
                 <div className="collapse navbar-collapse navbar-ex1-collapse">
                     <ul className="nav navbar-nav navbar-right">
-                        <li><button onClick={logout} className="btn btn-default navbar-btn">Sign out</button></li>
+                        <li><a className="auth" href="#"><img src={props.photoURL} alt=""></img>{props.displayName}</a></li>
+                        <li><button onClick={props.logout} className="btn btn-default navbar-btn">Sign out</button></li>
                     </ul>
                 </div>
+        </div>
             </nav>
     );
 }
